f47d4b946b69bbf49312341fc6660aaa9b03ee13
栖居 沈 <2104357372@qq.com>
2023-08-01 23:05:18 +0800

    bug fixed

--- services/game/akinator.py
+++ services/game/akinator.py
@@ -3,6 +3,7 @@
 # -- third party --
 from third_party.akinator import CantGoBackAnyFurther
 from third_party.akinator.async_aki import Akinator as Aki
+from aiohttp import ClientSession, TCPConnector
 
 # -- own --
 from .base import Game, GameBehavior, register_to_game
@@ -30,6 +31,8 @@ def ch_to_en(ans: str):
 
 
 graph: dict[int, ChronosItem] = {}
+CONN=TCPConnector(verify_ssl=False)
+COOL_DOWN_SEC=300
 
 
 @register_to_game("akinator")
@@ -39,13 +42,13 @@ class Akinator(Game):
 
     async def __setup(self):
         if item := graph.get(self.owner_id):
-            m = f"冷却中({600-item.elapsed_time:.0f}s)"
+            m = f"冷却中({COOL_DOWN_SEC-item.elapsed_time:.0f}s)"
         else:
-            graph[self.owner_id] = ChronosItem(self.owner_id, 600, lambda: graph.pop(self.owner_id))
+            graph[self.owner_id] = ChronosItem(self.owner_id, COOL_DOWN_SEC, lambda: graph.pop(self.owner_id))
             m = "tips:\n回复请使用(是|否|不知道|或许是|或许不是)\n退回上一问题请使用(/back)\n结束游戏请使用(/end)"
             await SendGroupMsg(self.group_id, m).do()
             self.aki = aki = Aki()
-            if not (m := await aki.start_game("cn", child_mode=True)):
+            if not (m := await aki.start_game("cn", child_mode=True,client_session=ClientSession(trust_env=True,connector=CONN))):
                 m = f"啊哦Σ(⊙▽⊙，{ACCIO.bot.name}似乎发生了一些错误，请稍后重试。"
                 self.kill()
         await SendGroupMsg(self.group_id, m).do()
@@ -53,10 +56,10 @@ class Akinator(Game):
 
 class AkinatorBehavior(GameBehavior[Akinator]):
     interested = [GroupMessage]
-    entrys = [r"^(?P<ans>是|不是|否|不知道|或许是|可能是|或许不是|可能不是|/back)$"]
+    entrys = [r"^(?P<ans>是|不是|否|不知道|或许是|可能是|或许不是|可能不是|/back|/end)$"]
 
     def check(self, evt: GroupMessage) -> bool:
-        return evt.user_id == self.game.owner_id and self.filter(evt) is not None
+        return (evt.user_id == self.game.owner_id) and self.filter(evt) is not None
 
     async def handle(self, evt: GroupMessage):
         if not (r := self.filter(evt)):
@@ -65,20 +68,22 @@ class AkinatorBehavior(GameBehavior[Akinator]):
         ans = ch_to_en(r["ans"])
         if ans == "/back":
             try:
-                m = await aki.back()
+                m = await aki.back() or "No Answer"
             except CantGoBackAnyFurther:
                 m = "回退不能。"
         elif ans == "/end":
             self.game.kill()
             graph.pop(self.game.owner_id)
-        else:
-            m = await aki.answer(ans)
+            m = f"游戏结束({self.game.owner_id})"
 
-        if aki.progression and aki.progression > 80:
+        elif aki.progression and aki.progression > 80:
             await aki.win()
             self.game.kill()
             if guess := aki.first_guess:
                 m = f"它应该是：{guess['name']} ({guess['description']})[CQ:image,file={guess['absolute_picture_path']}]"
             else:
                 m = "我猜不出来了，你赢了。"
-            await SendGroupMsg(self.game.group_id, m).do()
+        else:
+            m = await aki.answer(ans) or "No Answer"
+
+        await SendGroupMsg(self.game.group_id, m).do()
